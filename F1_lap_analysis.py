a0ab4f0637334a9b4299d457471a1fd28a1afeac
Ravi Sheoran <ravisheoranr@gmail.com>
2019-03-17 20:11:21 +0530

    Add files via upload

---
+++ F1_lap_analysis.py
@@ -0,0 +1,79 @@
+import PyPDF2
+import re
+import matplotlib.pyplot as plt
+
+FIG_SIZE = (8,6)
+FILE = 'N:/Downloads/race_laps.pdf'
+DRIVERS = ['Daniel RICCIARDO', 'Lando NORRIS', 
+                'Sebastian VETTEL', 'Kimi RAIKKONEN',
+                'Romain GROSJEAN', 'Pierre GASLY',
+                'Sergio PEREZ', 'Charles LECLERC',
+                'Lance STROLL', 'Kevin MAGNUSSEN',
+                'Alexander ALBON', 'Daniil KVYAT',
+                'Nico HULKENBERG', 'Max VERSTAPPEN',
+                'Lewis HAMILTON', 'Carlos SAINZ',
+                'George RUSSELL', 'Valtteri BOTTAS',
+                'Robert KUBICA', 'Antonio GIOVINAZZI']
+
+def get_lap_times(pdfReader, drivers):
+    num_pages =  pdfReader.numPages
+    # store driver names and their lap times in a list
+    lap_times = []
+    for i in range(num_pages):
+        pageObj = pdfReader.getPage(i)
+        text = pageObj.extractText()
+        # Use regex to search for drivers and lap times in the pdf text
+        lap_times += re.findall('|'.join(drivers)+
+                                '|[0-9].[0-9][0-9].[0-9][0-9][0-9]', text)
+        
+    # Create a dictionary to store a driver's lap times
+    # after converting them into float seconds
+    curr_driver = lap_times[0]
+    driver_lap_times = {}
+    
+    for lap in lap_times:
+        try:
+            secs = float(lap[2:])
+            secs = secs + float(lap[0])*60
+            driver_lap_times[curr_driver].append(round(secs, 3))
+        except ValueError:
+            curr_driver = lap
+            driver_lap_times[curr_driver] = []
+        
+    return driver_lap_times
+
+def plot_times(lap_times):
+    drivers_of_interest = ['Sebastian VETTEL', 'Charles LECLERC',
+                       'Lewis HAMILTON', 'Valtteri BOTTAS',
+                       'Max VERSTAPPEN']
+
+#    drivers_of_interest = ['Charles LECLERC',
+#                           'Max VERSTAPPEN']    
+    
+    # Configure pyplot
+    plt.style.use('tableau-colorblind10')
+    plt.figure(figsize=FIG_SIZE)
+    plt.xlabel('Lap Number')
+    plt.ylabel('Lap Time (seconds)')
+    
+    # change ylim and xticks based on race track
+    plt.ylim((85,95))
+    plt.xticks([i for i in range(0, 60, 5)], rotation=90)
+    for driver, times in lap_times.items():
+        if driver in drivers_of_interest:
+            plt.plot([i for i in range(len(times))], times, 'o-',
+                      linewidth=1, markersize=2.5,
+                      label=driver.split(' ')[1][:3])
+#            plt.text(len(times)-1, times[-1], driver.split(' ')[1][:3],
+#                   fontsize=10)
+        
+    plt.legend()
+
+if __name__ == '__main__':
+    pdfFileObj = open(FILE, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+    # Get the lap times
+    lap_times = get_lap_times(pdfReader, DRIVERS)
+
+    plot_times(lap_times)
